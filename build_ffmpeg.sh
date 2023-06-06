54203ebbb4bdb907675721d44529d6aa18970746
Ung, Teng En <teng.en.ung@intel.com>
2023-06-06 14:21:30 +0800

    Add ffmpeg build steps.

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -5,9 +5,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-BuildType=debug
-[ "$1"=="Release" ] && BuildType=release
-[ "$1"=="release" ] && BuildType=release
+ExtraOpts=
+[ "$1" == "Release" ] && ExtraOpts=" --disable-debug"
+[ "$1" == "release" ] && ExtraOpts=" --disable-debug"
 
 # *** Change the install directory prefix and proxies as you wish  ***
 # set http_proxy=
@@ -15,8 +15,8 @@ BuildType=debug
 InstallDirectoryPrefix=$(pwd)/_install
 
 INSTALL_DIR=$InstallDirectoryPrefix
-[ "$1"=="Release" ] && INSTALL_DIR=$InstallDirectoryPrefix/Release
-[ "$1"=="Debug" ] && INSTALL_DIR=$InstallDirectoryPrefix/Debug
+[ "$1" == "Release" ] && INSTALL_DIR=$InstallDirectoryPrefix/Release
+[ "$1" == "Debug" ] && INSTALL_DIR=$InstallDirectoryPrefix/Debug
 
 
 mkdir _extra
@@ -26,6 +26,6 @@ fi
 
 mkdir _build/$1/ffmpeg
 cd _build/$1/ffmpeg
-# PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig ../../../_extra/ffmpeg/configure  --target-os=win64 --arch=x86_64 --enable-vaapi --enable-libmfx --enable-shared --disable-static --toolchain=msvc
-PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig ../../../_extra/ffmpeg/configure  --target-os=win64 --arch=x86_64 --enable-vaapi --enable-shared --disable-static --toolchain=msvc
+# PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig ../../../_extra/ffmpeg/configure --prefix=$INSTALL_DIR --target-os=win64 --arch=x86_64 --enable-vaapi --enable-libmfx --enable-d3d11va $ExtraOpts --enable-shared --disable-static --toolchain=msvc
+PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig ../../../_extra/ffmpeg/configure --prefix=$INSTALL_DIR --target-os=win64 --arch=x86_64 --enable-vaapi --enable-d3d11va $ExtraOpts --enable-shared --disable-static --toolchain=msvc
 make
