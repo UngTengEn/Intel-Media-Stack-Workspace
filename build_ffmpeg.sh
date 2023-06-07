fc35f5effe63d89708d94d08bda305aa1bdb4362
Ung, Teng En <teng.en.ung@intel.com>
2023-06-07 15:43:15 +0800

    Correct the "\" for the install path in msys

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -8,7 +8,10 @@ if [ ! -e _extra/ffmpeg ]; then
     git clone --depth 1 --branch master https://github.com/FFmpeg/FFmpeg.git _extra/ffmpeg
 fi
 
+# Correct the "\" characters in path string
+InstallDir=$(echo $INSTALL_DIR | sed -e 's/\\/\//g')
+
 mkdir _build/$BuildDir/ffmpeg
 cd _build/$BuildDir/ffmpeg
-PKG_CONFIG_PATH="$PKG_CONFIG_DIR" ../../../_extra/ffmpeg/configure --prefix=$INSTALL_DIR --target-os=win64 --arch=x86_64 --enable-vaapi --enable-libvpl --enable-d3d11va $ExtraOpts --enable-shared --disable-static --toolchain=msvc
+PKG_CONFIG_PATH="$PKG_CONFIG_DIR" ../../../_extra/ffmpeg/configure --prefix="$InstallDir" --target-os=win64 --arch=x86_64 --enable-vaapi --enable-libvpl --enable-d3d11va $ExtraOpts --enable-shared --enable-static --toolchain=msvc
 make -j$(nproc - 1) install
