51af12fcfa7fec51050884d617322df19a19990a
Ung, Teng En <teng.en.ung@intel.com>
2023-06-10 09:14:54 +0800

    Don't build gstreamer if build folder exist and no openh264

--- build_gstreamer.bat
+++ build_gstreamer.bat
@@ -8,8 +8,10 @@ if not exist _extra\gstreamer\ (
     git clone --depth 1 --branch main https://gitlab.freedesktop.org/gstreamer/gstreamer.git _extra\gstreamer
 ) 
 
-meson setup _build\%BuildDir%\gstreamer  _extra\gstreamer --prefix "%INSTALL_DIR%" --buildtype %BuildType% -Dpkg_config_path="%PKG_CONFIG_DIR%" -Dlibav=disabled -Ddevtools=disabled -Dges=disabled -Drtsp_server=disabled -Dgst-examples=disabled -Dtls=disabled -Dtests=disabled -Dexamples=disabled -Dgst-plugins-base:gl=disabled -Dgst-plugins-bad:gl=disabled -Dgst-plugins-bad:msdk=enabled -Dgst-plugins-bad:va=enabled -Dcairo:tests=disabled
-ninja -C  _build\%BuildDir%\gstreamer install
+if not exist _build\%BuildDir%\gstreamer (
+    meson setup _build\%BuildDir%\gstreamer  _extra\gstreamer --prefix "%INSTALL_DIR%" --buildtype %BuildType% -Dpkg_config_path="%PKG_CONFIG_DIR%" -Dlibav=disabled -Ddevtools=disabled -Dges=disabled -Drtsp_server=disabled -Dgst-examples=disabled -Dtls=disabled -Dtests=disabled -Dexamples=disabled -Dgst-plugins-base:gl=disabled -Dgst-plugins-bad:gl=disabled -Dgst-plugins-bad:openh264=disabled -Dgst-plugins-bad:msdk=enabled -Dgst-plugins-bad:va=enabled -Dcairo:tests=disabled
+    ninja -C  _build\%BuildDir%\gstreamer install
+)
 
 goto end
 
