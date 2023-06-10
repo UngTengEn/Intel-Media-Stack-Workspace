e9fb243dd7be75a9a4fdd589c9f62f925e24fcc3
Ung, Teng En <teng.en.ung@intel.com>
2023-06-10 09:12:17 +0800

    Default to x64 build

--- build_env.bat
+++ build_env.bat
@@ -12,6 +12,6 @@ rem set https_proxy=
 rem *** Setup MSVC environments.  Have to put at end, or will override previous settings.      ***
 rem *** The path to vcvarsall.bat depends on the version of Visual Studio you have installed.  ***
 rem *** Configure build arch (x86, x64, x86_x64, x64_x86).                                     ***
-"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
+"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 @ECHO ON
