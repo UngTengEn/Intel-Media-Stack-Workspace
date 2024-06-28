eaae2654c3dd23635572b986cb8f224216d68246
Ung, Teng En <teng.en.ung@intel.com>
2024-06-28 11:46:01 +0800

    Fix gstreamer extra libav build scripts

--- build_extra_gst-libav.bat
+++ build_extra_gst-libav.bat
@@ -1,7 +1,32 @@
 @ECHO OFF
 
-if not exist _build\%BuildDir%\gst-libav (
-    meson setup _build\%BuildDir%\gst-libav  _extra\gstreamer\subprojects\gst-libav  --prefix "%INSTALL_DIR%" --buildtype %BuildType% -Dpkg_config_path="%PKG_CONFIG_DIR%" -Dtests=disabled -Ddoc=disabled
-    ninja -C _build\%BuildDir%\gst-libav install
+if "%1"=="" goto help
+
+set BuildType=Debug
+set BuildDir=Debug
+set buildtype=debug
+
+if "%1%"=="RELEASE" set BuildType=Release
+if "%1"=="Release" set BuildType=Release
+if "%1"=="release" set BuildType=Release
+if "%BuildType%"=="Release" set buildtype=release
+
+if "%BuildType%"=="Release" set BuildDir=Release
+
+if not exist _build\%BuildDir%\gst-plg-libav/build.ninja (
+    meson setup _build\%BuildDir%\gst-plg-libav  _extra\gstreamer\subprojects\gst-libav  --prefix "%INSTALL_DIR%" --buildtype %BuildType% -Dpkg_config_path="%PKG_CONFIG_DIR%" -Dtests=disabled -Ddoc=disabled
+    ninja -C _build\%BuildDir%\gst-plg-libav install
 )
+
+goto end
+
+:help
+echo Usage: build_extra_gst-libav.bat [debug|release]
+
+:end
+
+set BuildType=
+set buildtype=
+set BuildDir=
+
 @ECHO ON
